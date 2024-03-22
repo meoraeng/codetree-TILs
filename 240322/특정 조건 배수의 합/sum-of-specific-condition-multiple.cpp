c6c6693227a6208af951daca5418bcb7c4c1cafb
KIM SEON WOO <sunwoo005@naver.com>
2024-03-22 13:56:04 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240322/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 240322/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main() {
     int a,b;
-    int sum =0;
+    int sum = 0;
     cin >> a;
     cin >> b;
 
     for(int i=a; i<(b+1); i++){
         if(i%5==0) {
+            cout << "이건 하냐";
             sum += i;
         }
     }
