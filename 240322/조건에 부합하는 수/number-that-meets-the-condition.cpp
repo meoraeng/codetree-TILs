44d1065633d16a99784b9e998bd6d49e7147e857
KIM SEON WOO <sunwoo005@naver.com>
2024-03-22 14:36:52 +0900

    Title: 조건에 부합하는 수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240322/조건에 부합하는 수/number-that-meets-the-condition.cpp
+++ 240322/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -4,9 +4,11 @@ int main() {
     int a;
     cin >> a;
     for(int i=1; i<=a; i++){
-        if(!((i%2==0&&i%4!=0)||(i/8)%2==0||(i%7)<4)){
-            cout << i << " ";
-        }
+        if((i%2==0&&i%4!=0)||(i/8)%2==0||(i%7)<4){
+            continue;
+        }    
+        cout << i << " ";
     }
+    
     return 0;
 }
