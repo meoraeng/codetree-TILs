c5c028a91b4646c26122e9717856440ae9ebe61b
KIM SEON WOO <sunwoo005@naver.com>
2024-03-22 13:15:52 +0900

    Title: a부터 b까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240322/a부터 b까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    int sum = 0;
+    cin >> a;
+    cin >> b;
+
+    for(int i=a; i<=b; i++){
+        sum += i;
+    }
+    cout << sum;
+
+    return 0;
+}
