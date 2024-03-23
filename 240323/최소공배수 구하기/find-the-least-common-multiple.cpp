f3e5a59dd8da75a23d9136f96e84cd02a739fa73
KIM SEON WOO <sunwoo005@naver.com>
2024-03-23 22:24:51 +0900

    Title: 최소공배수 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240323/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int maxProd(int a, int b){
+    if(b==0) {
+        return a;
+    }
+    else{
+        return maxProd(b,a%b);
+    }
+}
+int main() {
+    int n,m;
+    cin >> n >> m;
+    int a,b;
+    if(n>m){
+        a=n;
+        b=m;
+    } else{
+        a=m;
+        b=n;
+    }
+    cout << (a*b)/maxProd(a,b);
+    
+    return 0;
+}
