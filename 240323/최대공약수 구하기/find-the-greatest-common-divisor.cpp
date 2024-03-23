5e35b669c686b019a1926db4816471bc73ff1744
KIM SEON WOO <sunwoo005@naver.com>
2024-03-23 21:47:02 +0900

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240323/최대공약수 구하기/find-the-greatest-common-divisor.cpp
+++ 240323/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -1,26 +1,13 @@
 #include <iostream>
 using namespace std;
 int maxProd(int a, int b){
-    int mod;
-    int result;
-    if(a>b){
-        mod = a%b;
-        if(mod==0) return b;
-    }else if(a<b){
-        mod = b%a;
-        if(mod==0) return a;
+    int mod = b % a;
+    if(mod==0) return a;
+    int result = 0;
+    while(result != 0){
+        result = a % mod;
     }
-    for(;;){
-        if(a>b){
-            result = b%mod;
-            if(result==0) return mod;
-        }else if(a<b){
-            result =  a%mod;
-            if(result==0) return mod;
-        }
-        
-    }
-
+    return mod;
 }
 int main() {
     int n,m;
